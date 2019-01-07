8d965e43c5e3e3bd890ca7551f5507fa88b516ec
Kozicki Matthieu <matthieu.kozicki@epitech.eu>
2019-01-07 12:30:58 +0100

    [adding] no flag fucntion

--- src/main.c
+++ src/main.c
@@ -7,12 +7,33 @@
 
 #include <stdarg.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
 #include <stdlib.h>
 #include "my.h"
 
+void flag_a(char *arg)
+{
+    struct dirent *file;
+    DIR *rep = opendir(arg);
+}
+
+void no_flag(void)
+{
+    struct dirent *file;
+    DIR *rep = opendir(".");
+
+    while (file = readdir(rep)) {
+        if (file->d_name[0] != '.')
+            my_printf("%s  ", file->d_name);
+    }
+    my_printf("\n");
+    closedir(rep);
+}
+
 int main(int arc, char **arg)
 {
-    int i = 0;
-    my_printf("%d", i);
+    if (arc == 1)
+        no_flag();
     return (0);
 }
