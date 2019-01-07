088abf33b93ec85c3af5522cbec8287e6612b2c6
Kozicki Matthieu <matthieu.kozicki@epitech.eu>
2019-01-07 13:06:28 +0100

    [adding] flag a and ls with out flag but path

--- src/main.c
+++ src/main.c
@@ -16,6 +16,24 @@ void flag_a(char *arg)
 {
     struct dirent *file;
     DIR *rep = opendir(arg);
+
+    while (file = readdir(rep))
+        my_printf("%s ",file->d_name);
+    my_printf("\n");
+    closedir(rep);
+}
+
+void no_flag_path(char *arg)
+{
+    struct dirent *file;
+    DIR *rep = opendir(arg);
+
+    while (file = readdir(rep)) {
+        if (file->d_name[0] != '.')
+            my_printf("%s  ", file->d_name);
+    }
+    my_printf("\n");
+    closedir(rep);
 }
 
 void no_flag(void)
@@ -31,9 +49,20 @@ void no_flag(void)
     closedir(rep);
 }
 
+int parser(int arc, char **arg)
+{
+    if (arg[1][0] != '-')
+        no_flag_path(arg[1]);
+    if (arg[1][1] == 'a')
+        flag_a(arg[2]);
+}
+
 int main(int arc, char **arg)
 {
     if (arc == 1)
         no_flag();
+    else {
+        parser(arc, arg);
+    }
     return (0);
 }
